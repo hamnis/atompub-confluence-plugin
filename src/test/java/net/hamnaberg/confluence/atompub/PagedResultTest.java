8073a3ba3f02c84929b1c57a7f8a2e7498a31eb1
Erlend <ngarthl@gmail.com>
2011-02-10 14:06:30 +0100

    - copyright

--- src/test/java/net/hamnaberg/confluence/atompub/PagedResultTest.java
+++ src/test/java/net/hamnaberg/confluence/atompub/PagedResultTest.java
@@ -1,3 +1,19 @@
+/*
+ * Copyright 2011 Erlend Hamnaberg
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
 package net.hamnaberg.confluence.atompub;
 
 import org.apache.abdera.model.Link;
@@ -6,13 +22,7 @@ import org.junit.Test;
 
 import javax.ws.rs.core.UriBuilder;
 
-/**
- * Created by IntelliJ IDEA.
- * User: maedhros
- * Date: 1/22/11
- * Time: 5:26 PM
- * To change this template use File | Settings | File Templates.
- */
+
 public class PagedResultTest {
     protected static final String LINK_TEMPLATE = "<link xmlns=\"http://www.w3.org/2005/Atom\" rel=\"%s\" href=\"/?pw=%s\" />";
 
